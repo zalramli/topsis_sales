e9851a862921c6c71bd5601534ca3e54d6d0ed48
Rizal Ramli <github.little@gmail.com>
2020-04-06 10:28:56 +0700

    pasang template

---
+++ _partials/javascript.php
@@ -0,0 +1,14 @@
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="assets/scripts/klorofil-common.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('#example').DataTable({
+        "ordering": false
+    });
+});
+</script>
---
+++ admin.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<?php include "_partials/head.php"; ?>
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		<!-- NAVBAR -->
+		<?php include "_partials/header.php"; ?>
+		<!-- END NAVBAR -->
+		<!-- LEFT SIDEBAR -->
+		<?php include "_partials/sidebar.php" ?>
+		<!-- END LEFT SIDEBAR -->
+		<!-- MAIN -->
+		<div class="main">
+			<!-- MAIN CONTENT -->
+			<div class="main-content">
+            <?php 
+                if(!isset($_GET['halaman'])) {
+                        error_reporting(0);
+                    }
+                    // if ($_GET['halaman'] == 'dashboard') {
+                    //     include "dashboard/dashboard_admin.php";
+                    // }
+                    // Tutup Dashboard
+
+                    // Parsing halaman Pegawai
+                    if ($_GET['halaman'] == 'karyawan') {
+                        include "system/master/karyawan/tampil.php";
+                    }
+                ?>
+			</div>
+			<!-- END MAIN CONTENT -->
+		</div>
+		<!-- END MAIN -->
+		<div class="clearfix"></div>
+		<footer>
+			<div class="container-fluid">
+				<p class="copyright">Shared by <i class="fa fa-love"></i><a href="https://bootstrapthemes.co">BootstrapThemes</a></p>
+			</div>
+		</footer>
+	</div>
+	<!-- END WRAPPER -->
+	<!-- Javascript -->
+	<?php include "_partials/javascript.php"; ?>
+</body>
+
+</html>
